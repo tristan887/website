91592b6390bf6b995f4ffd8175076084d03133e5
Diana Tristan <dianatristan@MacBookPro.lan>
2025-10-03 17:17:54 -0400

    update page.js and route.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
+// font
+
+import { Playfair_Display, Montserrat } from 'next/font/google'
+
+const playfair = Playfair_Display({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
+
 // frontend - runs the browser :D
 export default function Home() {
   return (
-    <main className="min-h-screen p-8">
-      <h1 className="text-4xl font-bold">My Portfolio</h1>
-      <p className="mt-4">Welcome to my finance and economics projects.</p>
+    <main className="min-h-screen flex flex-col justify-center items-center p-8">
+      <h1 className={`text-8xl font-black tracking-tight ${playfair.className}`}>
+        Diana Tristan
+      </h1>
+      {/* <h1 className="text-8xl font-black tracking-tight font-serif">My Portfolio</h1> */}
+      <p className='mt-4 ${montserrat.className}`}'>
+        Welcome to my finance and economics projects.</p>
+      <p className="mt-6">Hi! My name is Diana and I am a senior at the University of Central Florida.
+        I will be graduating with my Bachelor's in Economics and Minor in Middle Easter Studies.
+      </p>
 
       {/* Option 2: Multiple buttons */}
       <div className="mt-6 flex gap-4">
         <button className="bg-blue-500 text-white px-6 py-2 rounded">
           Projects
         </button>
-        <button className="bg-green-500 text-white px-6 py-2 rounded">
+        <button className="bg-red-500 text-white px-6 py-2 rounded">
           Contact
         </button>
       </div>
